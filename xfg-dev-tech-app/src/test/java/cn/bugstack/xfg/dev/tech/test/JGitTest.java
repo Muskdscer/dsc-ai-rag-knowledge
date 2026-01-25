b278628dcd7675a121c7fca26c6510ff1da33929
DSC <D695312811@163.com>
2026-01-25 18:49:29 +0800

    feat:8-rag-git 自动拉取仓库

---
+++ xfg-dev-tech-app/src/test/java/cn/bugstack/xfg/dev/tech/test/JGitTest.java
@@ -0,0 +1,79 @@
+package cn.bugstack.xfg.dev.tech.test;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.PathResource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JGitTest {
+
+    //Ykh4v36gfya4_-4DdiHdzHp6
+    @Resource
+    private OllamaChatClient ollamaChatClient;
+    @Resource
+    private TokenTextSplitter tokenTextSplitter;
+    @Resource
+    private SimpleVectorStore simpleVectorStore;
+    @Resource
+    private PgVectorStore pgVectorStore;
+
+    @Test
+    public void test() throws IOException, GitAPIException {
+        // 这部分替换为你的
+        String repoURL = "https://gitcode.com/DDDDSSSSCCCC/xfg-dev-tech-http";
+        String username = "DDDDSSSSCCCC";
+        String password = "Ykh4v36gfya4_-4DdiHdzHp6";
+
+        String localPath = "./cloned-repo";
+        log.info("克隆路径：" + new File(localPath).getAbsolutePath());
+
+        FileUtils.delete(new File(localPath));
+        Git git = Git.cloneRepository().setURI(repoURL)
+                .setDirectory(new File(localPath))
+                .setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password))
+                .call();
+        git.close();
+    }
+
+    @Test
+    public void test_file() throws IOException {
+        Files.walkFileTree(Paths.get("./cloned-repo"), new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                log.info("文件路径:{}", file.toString());
+
+                PathResource resource = new PathResource(file);
+                TikaDocumentReader reader = new TikaDocumentReader(resource);
+                List<Document> documents = reader.get();
+                List<Document> documentSplitterList = tokenTextSplitter.apply(documents);
+
+                documents.forEach(doc -> doc.getMetadata().put("knowledge", "group-buy-market-liergou"));
+                documentSplitterList.forEach(doc -> doc.getMetadata().put("knowledge", "group-buy-market-liergou"));
+                pgVectorStore.accept(documentSplitterList);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+}
